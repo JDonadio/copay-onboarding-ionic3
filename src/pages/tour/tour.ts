1c37c437bf6edfabdc010925101a551d6433672d
JDonadio <tanojaja@gmail.com>
2017-07-28 17:55:13 -0300

    add first sliders

--- src/pages/tour/tour.ts
+++ src/pages/tour/tour.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions';
 
 /**
  * Generated class for the TourPage page.
